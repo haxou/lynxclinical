c49890ef611b803044b6179ddedaec5466ee3e02
Arnav Ghosh <67281372+moimoifi@users.noreply.github.com>
2024-11-28 15:11:06 -0500

    Update game1.js

--- game1.js
+++ game1.js
@@ -67,7 +67,7 @@ class Person {
     }
 }
 let swimmers = [],
-    timeCount = 10,
+    timeCount = 15,
     start = Date.now()
 const interv = () => {
     var delta = Date.now() - start // milliseconds elapsed since start
